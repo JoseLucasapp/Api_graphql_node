8d3d79b05d850688f2824f19b990f983a7ee7e38
José Lucas <josluc2016@gmail.com>
2021-11-19 15:36:28 -0300

    feat: first mutation

--- src/index.js
+++ src/index.js
@@ -3,12 +3,57 @@ const {ApolloServer, gql} = require('apollo-server');
 const typeDefs = gql`
     type Query{
         hello: String
+        users: [User!]!
+        getUserByEmail(email: String!): User!
+    }
+
+    type User{
+        _id: ID!
+        name: String!
+        email: String!
+        active: Boolean!
+    }
+
+    type Post{
+        _id: ID!
+        title: String
+        content: String
+        author: User!
+    }
+
+    type Mutation{
+        createUser(email: String!, name: String!): User!
     }
 `;
 
+const users = [
+    {_id: String(Math.random()), name: 'Lucas', email:'lucas@gmail.com', active: true},
+    {_id: String(Math.random()), name: 'Lucas', email:'lucas2@gmail.com', active: true},
+    {_id: String(Math.random()), name: 'Lucas', email:'lucas3@gmail.com', active: false},
+]
+
 const resolvers = {
     Query: {
-        hello: ()=> 'hello world'
+        hello: ()=> 'hello world',
+        users: ()=> users,
+        getUserByEmail: (_, args)=>{
+            return users.find((user)=> user.email === args.email)
+        }
+    },
+
+    Mutation: {
+        createUser: (_, args)=>{
+            const newUser = {
+                _id: String(Math.random()),
+                name: args.name,
+                email: args.email,
+                active: true
+            }
+
+            users.push(newUser);
+
+            return newUser;
+        }
     }
 };
 
